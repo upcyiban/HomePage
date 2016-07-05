adfc05f53138e51db60f4e27a5fafcc06ca29835
wulei123 <1534563365@qq.com>
2016-07-05 21:27:22 +0800

    homePage第一版块完成

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,27 +1,5 @@
 console.log('\'Allo \'Allo!');
 $(document).ready(function () {
-showSection();
-$('a').on('click',function(){
-  $('.showSection').removeClass('showSection-show');
-  $('.showSection').addClass('showSection-show');
-  });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-  function showSection() {
-    $('.showSection').removeClass('showSection-show');
-    $('.showSection').addClass('showSection-show');
-  }
 
 });
