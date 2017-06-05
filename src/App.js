dfd535f46097876e9549e61e95b88bd6fe3ca65a
wulei123 <1534563365@qq.com>
2017-06-05 12:09:44 +0800

    add footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import AppSection from './components/app-section/AppSection'
 import InfoSection from "./components/info-section/InfoSection"
 import RightBar from "./components/right-bar/RightBar"
 import TitleBar from "./components/title-bar/TitleBar"
+import Footer from './components/footer/Footer'
 import './App.css';
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
           <TitleBar />
           <InfoSection />
           <AppSection />
+          <Footer />
           <RightBar />
       </div>
     );
